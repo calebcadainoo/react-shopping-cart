4173d83c6608c8253d716cfe870661d444da1073
Caleb Crosby Adainoo <56970927+calebcadainoo@users.noreply.github.com>
2020-07-19 16:51:41 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      {/* page header */}
+      <header className="page-header">
+        <a href="/">React Shopping Cart</a>
       </header>
+
+      {/* page section */}
+      <section className="page-section">
+        Product List
+      </section>
+
+      {/* page footer */}
+      <footer className="page-footer">
+        All rights reserved.
+      </footer>
     </div>
   );
 }

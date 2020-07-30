23aebcd21c4434e1f60c41e4dd3ed7db4812a07e
Caleb Crosby Adainoo <56970927+calebcadainoo@users.noreply.github.com>
2020-07-30 17:35:25 +0000

    create order

--- server.js
+++ server.js
@@ -56,5 +56,43 @@ app.delete("/api/products/:id", async(req, res) => {
 	res.send(deletedProduct);
 });
 
+
+// second endpoint
+const Order = mongoose.model("order", new mongoose.Schema({
+	_id: { type: String, default: shortid.generate },
+	email: String,
+	name: String,
+	address: String,
+	total: Number,
+	cartItems: [{
+		_id: String,
+		title: String,
+		price: Number,
+		count: Number
+	}]
+}, { timestamps: true }));
+
+app.get("/api/orders", async (req, res) => {
+	const order = await Order.find({});
+	res.send(order);
+});
+
+app.post("/api/orders", async (req, res) => {
+	// check if all required data exists in body
+	if (!req.body.name || !req.body.email || !req.body.address || !req.body.total || !req.body.cartItems) {
+		return res.send({
+			message: "Data is required"
+		});
+	}
+
+	const order = await Order(req.body).save();
+	res.send(order);
+});
+
+app.delete("/api/orders/:id", async(req, res) => {
+	const deletedOrder = await Order.findByIdAndDelete(req.params.id);
+	res.send(deletedOrder);
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("serve at http:://localhost:5000"));
